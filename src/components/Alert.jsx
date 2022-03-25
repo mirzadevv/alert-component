abaace3c7d1a2845c45a5ee38a589292e5a8fa1e
mirzaei.dev <mirzaei.devv@gmail.com>
2022-03-25 19:12:06 +0430

    feature: add the type property for showing the appropriate alert
    Type property can be included success, info, error, and warning.
    according to each case, we change its color in css.

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import "./Alert.css";
 
-function Alert({ message, duration = 2500 }) {
+function Alert({ message, type = "success", duration = 2500 }) {
   const [showAlert, setShowAlert] = useState(false);
+
   useEffect(() => {
     setTimeout(() => {
       setShowAlert(true);
@@ -15,9 +16,9 @@ function Alert({ message, duration = 2500 }) {
   return (
     <div className="alert-container">
       {showAlert ? (
-        <div className={`alert show-alert`}>{message}</div>
+        <div className={`alert ${type}-alert show-alert`}>{message}</div>
       ) : (
-        <div className={`alert hide-alert`}> {message} </div>
+        <div className={`alert ${type}-alert hide-alert`}> {message} </div>
       )}
     </div>
   );
