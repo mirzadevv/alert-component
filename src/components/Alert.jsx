700ecc1bcc1990c40a0d79dfd13813b098473887
mirzaei.dev <mirzaei.devv@gmail.com>
2022-03-25 19:22:42 +0430

    feature: add a closable property for having a close button into the alert
    The default value of this property is false if it would be true so the alert wouldn't be hidden and the close button will appear.
    by clicking on the close button, the state of showAlert will be false, and the alert would be hidden.

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,7 +1,12 @@
 import React, { useEffect, useState } from "react";
 import "./Alert.css";
 
-function Alert({ message, type = "success", duration = 2500 }) {
+function Alert({
+  message,
+  type = "success",
+  duration = 2500,
+  closable = false,
+}) {
   const [showAlert, setShowAlert] = useState(false);
 
   useEffect(() => {
@@ -9,14 +14,26 @@ function Alert({ message, type = "success", duration = 2500 }) {
       setShowAlert(true);
     }, 100);
     setTimeout(() => {
-      setShowAlert(false);
+      !closable && setShowAlert(false);
     }, duration);
   }, []);
+
+  const handleClose = () => {
+    setShowAlert(false);
+  };
 
   return (
     <div className="alert-container">
       {showAlert ? (
-        <div className={`alert ${type}-alert show-alert`}>{message}</div>
+        <div className={`alert ${type}-alert show-alert`}>
+          {message}
+          {closable && (
+            <div className="close" onClick={handleClose}>
+              <div className="first-line"></div>
+              <div className="second-line"></div>
+            </div>
+          )}
+        </div>
       ) : (
         <div className={`alert ${type}-alert hide-alert`}> {message} </div>
       )}
