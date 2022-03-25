d6ee53d4909f915a99dbb8a24445799d3505fbde
mirzaei.dev <mirzaei.devv@gmail.com>
2022-03-25 19:15:40 +0430

    send type property to the Alert component
    Type property can be included success, info, error, and warning.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Alert from "./components/Alert";
 function App() {
-  return <Alert message="Success message" duration={4000} />;
+  return <Alert message="Success message" duration={4000} type="info" />;
 }
 
 export default App;
